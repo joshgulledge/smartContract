d286b06258d5d873ce6097205c98e05d922c393b
joshgulledge <joshcalebgulledge@gmail.com>
2021-12-11 21:13:30 -0600

    starting basic smart contract project

---
+++ contracts/inbox.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    // storage variable
+    string public message;
+
+    // constructor function
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    // normal functions
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+}
