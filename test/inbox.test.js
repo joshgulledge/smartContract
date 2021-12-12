227f3bb353ffefbfd5537dd0ed0ed9399fbe2428
joshgulledge <joshcalebgulledge@gmail.com>
2021-12-11 21:38:09 -0600

    did some quick mocha testing review

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,3 +2,32 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
+const web3Inst = new Web3(ganache.provider());
+
+
+class Car {
+    park () {
+        return 'stopped';
+    };
+
+    drive () {
+        return 'vroom';
+    };
+}
+
+let car;
+
+beforeEach(() => {
+     car = new Car();
+});
+
+describe('Car Test', () => {
+    it('park method', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('drive method', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+
+});
