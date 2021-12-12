7f2856663e6116abcc5f530e00adef18105e620c
joshgulledge <joshcalebgulledge@gmail.com>
2021-12-12 00:50:46 -0600

    node run deploy.js to deploy contract to network

--- deploy.js
+++ deploy.js
@@ -16,3 +16,17 @@ const provider = new HdWalletProvider(
 // use provider obj to connet to network
 const web3 = new Web3(provider);
 
+// create a deploy function to use async/await
+const deploy = async function () {
+    const accounts = await web3.eth.getAccounts();
+    console.log(`attempting to deploy from ${accounts[0]}`);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello Network']})
+        .send({ gas: '1000000', from: accounts[0]});
+
+    console.log(`Deployed to ${result.options.address}`);
+};
+
+// call deploy
+deploy();
